48a102b90249de0bcdff168135284f366ea0d849
deping <757097134@qq.com>
2023-04-03 17:57:21 +0800

    feat: project initialize

---
+++ BaseApi/src/main/java/com/ping/po/TestPo.java
@@ -0,0 +1,14 @@
+package com.ping.po;
+
+/**
+ * ========================
+ * module_name:[]
+ * module_desc:
+ *
+ * @author: dp
+ * @date: 2023/4/3
+ * @since: 1.0
+ * ========================
+ */
+public class TestPo {
+}
