677ea452c356fd421e6a05dedde2cb10c309c8cc
deping <757097134@qq.com>
2023-04-06 09:55:57 +0800

    feat: add flink-streaming-java

---
+++ BaseApi/src/main/java/com/ping/watermark/WaterMarkDemo.java
@@ -0,0 +1,17 @@
+package com.ping.watermark;
+
+/**
+ * ========================
+ * module_name:[]
+ * module_desc:
+ *
+ * @author: dp
+ * @date: 2023/4/6
+ * @since: 1.0
+ * ========================
+ */
+public class WaterMarkDemo {
+    public static void main(String[] args) {
+        StreamExceE
+    }
+}
