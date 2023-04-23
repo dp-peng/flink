e1e3e2c84b2bce615117c4d05200466d0c42dede
deping <757097134@qq.com>
2023-04-23 21:56:02 +0800

    feat: CheckPointConfig

---
+++ BaseApi/src/main/java/com/ping/checkpoint/CheckPointConfig.java
@@ -0,0 +1,14 @@
+package com.ping.checkpoint;
+
+/**
+ * ========================
+ * module_name:[]
+ * module_desc:
+ *
+ * @date: 2023/4/23
+ * @since: 1.0
+ * ========================
+ */
+public class CheckPointConfig {
+
+}
